eeeb692a65d2b636a6ddaf586cc00a5079ca1c7e
kjy <visionjyk21@naver.com>
2020-08-05 14:40:49 +0900

    검색부분

---
+++ app/src/main/java/com/kidari/mrlonglegs/SearchItemAdapter.kt
@@ -0,0 +1,47 @@
+package com.kidari.mrlonglegs
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class SearchItemAdapter(val items: List<SearchItemMember>) : RecyclerView.Adapter<SearchItemAdapter.ViewHolder>()  {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.search_title_item_layout, parent, false)
+        return ViewHolder(view)
+    }
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+
+        holder.title_search.text = item.Search_item_title
+        holder.registraion_search.text = item.Search_item_registrationdate
+        holder.location_search.text = item.Search_item_location
+        holder.payment_serach.text = item.Search_item_pay
+
+        holder.itemView.setOnClickListener{
+            itemClickListener.onClick(it, position)
+        }
+    }
+
+    inner class ViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview) {
+        var title_search = itemview.findViewById<TextView>(R.id.search_title_title)
+        var registraion_search = itemview.findViewById<TextView>(R.id.search_title_registrationDate)
+        var location_search = itemview.findViewById<TextView>(R.id.search_title_location)
+        var payment_serach = itemview.findViewById<TextView>(R.id.search_title_payment)
+    }
+
+    interface ItemClickListener {
+        fun onClick(view: View, position: Int)
+    }
+
+    private lateinit var itemClickListener: ItemClickListener
+
+    fun setItemClickListener(itemClickListener: ItemClickListener){
+        this.itemClickListener = itemClickListener
+    }
+}
+
--- app/src/main/java/com/kidari/mrlonglegs/SearchTitleFragment.kt
+++ app/src/main/java/com/kidari/mrlonglegs/SearchTitleFragment.kt
@@ -1,24 +1,46 @@
 package com.kidari.mrlonglegs
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QueryDocumentSnapshot
+import kotlinx.android.synthetic.main.fragment_list.*
 import kotlinx.android.synthetic.main.fragment_search_title.*
 
 class SearchTitleFragment : Fragment() {
 
-    val items = ArrayList<ListItemMember>()
-    val recyclerAdapter = ListItemAdapter(items)
+    val db = FirebaseFirestore.getInstance()
+    val items = ArrayList<SearchItemMember>()
+    val recyclerAdapter = SearchItemAdapter(items)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_title, container, false)
+        val root = inflater.inflate(R.layout.fragment_search_title, container, false)
+        return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        searchTitleRecyclerView.layoutManager =
+            LinearLayoutManager(this.context, RecyclerView.VERTICAL, false)
+
+        recyclerAdapter.setItemClickListener(object : SearchItemAdapter.ItemClickListener{
+            override fun onClick(view: View, position: Int) {
+                val intent = Intent(context, DetailsActivity::class.java)
+                startActivity(intent)
+            }
+        })
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -26,8 +48,35 @@ class SearchTitleFragment : Fragment() {
         searchView.setOnSearchClickListener {
             val title = searchView.query.toString()
             // db에서 title 검색
+
         }
+       loadData()
         searchTitleRecyclerView.adapter = recyclerAdapter
+    }
+
+    //여기서 스트링 구별
+    fun loadData() {
+        db.collection("심부름")
+            .get()
+            .addOnSuccessListener { result ->
+                items.clear()
+                for (document in result) {
+                    items.add(toListItemMember(document))
+
+                    recyclerAdapter.notifyDataSetChanged()
+                    Log.d(TAG, "${document.id} => ${document.data["name"]}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "Error getting documents: ", exception)
+            }
+
+
+    }
+
+    fun toListItemMember(document: QueryDocumentSnapshot): SearchItemMember {
+        return SearchItemMember(document["title"].toString(), document["regDay"].toString(),
+            document["payment"].toString(), document["location"].toString())
     }
 
 }
