c202a4d023a628faa2742de9752def853f7ca0b3
hohoprince <hoot0512@gmail.com>
2020-08-11 17:30:07 +0900

    google map 삭제

--- app/src/main/java/com/kidari/mrlonglegs/Fragment/SurroundingsFragment.kt
+++ app/src/main/java/com/kidari/mrlonglegs/Fragment/SurroundingsFragment.kt
@@ -11,80 +11,16 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.kidari.mrlonglegs.R
 
 
-class SurroundingsFragment : Fragment(), OnMapReadyCallback {
-
-    private lateinit var mapView: MapView
+class SurroundingsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_surroundings, container, false)
-        mapView = root.findViewById(R.id.mapView) as MapView
-        mapView.getMapAsync(this)
-        return root
+        return inflater.inflate(R.layout.fragment_surroundings, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
     }
-
-    override fun onMapReady(googleMap: GoogleMap?) {
-        val SEOUL = LatLng(37.56, 126.97);
-
-        val markerOptions = MarkerOptions()
-
-        markerOptions.position(SEOUL);
-
-        markerOptions.title("서울");
-
-        markerOptions.snippet("수도");
-
-        googleMap?.addMarker(markerOptions);
-
-        googleMap?.moveCamera(CameraUpdateFactory.newLatLng(SEOUL))
-    }
-
-    override fun onStart() {
-        super.onStart()
-        mapView.onStart()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mapView.onStop()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView.onSaveInstanceState(outState)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mapView.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mapView.onPause()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView.onLowMemory()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView.onDestroy()
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        mapView.onCreate(savedInstanceState)
-    }
-
-
 }
