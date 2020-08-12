b34340b8c7c6b1b9924bf470d1b448bf1a9547f7
hohoprince <hoot0512@gmail.com>
2020-08-12 20:14:34 +0900

    심부름 수행 포기 횟수를 db에 업데이트

--- app/src/main/java/com/kidari/mrlonglegs/Activity/IngItemActivity.kt
+++ app/src/main/java/com/kidari/mrlonglegs/Activity/IngItemActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.firepush.Fire
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.kidari.mrlonglegs.R
 import kotlinx.android.synthetic.main.activity_details.*
@@ -48,6 +49,7 @@ class IngItemActivity : AppCompatActivity() {
             Toast.makeText(this, "완료되었습니다", Toast.LENGTH_SHORT).show()
             updateErrandState(id)
             updateSupporterOfErrand(id)
+            countComplete() // 완료 횟수 증가
             finish()
         }
 
@@ -62,59 +64,74 @@ class IngItemActivity : AppCompatActivity() {
             Toast.makeText(this, "포기되었습니다", Toast.LENGTH_SHORT).show()
             updategiveupErrandState(id)
             updategiveupSupporterOfErrand(id)
+            countGiveup() // 포기 횟수 증가
             finish()
         }
     }
 
-// 심부름에 서포터 정보 입력
-fun updateSupporterOfErrand(id: String) {
-    val user = FirebaseAuth.getInstance().currentUser
-    val sfDocRef = db.collection("심부름").document("$id")
+    // 심부름에 서포터 정보 입력
+    fun updateSupporterOfErrand(id: String) {
+        val user = FirebaseAuth.getInstance().currentUser
+        val sfDocRef = db.collection("심부름").document("$id")
 
-    db.runTransaction { transaction ->
-        val snapshot = transaction.get(sfDocRef)
+        db.runTransaction { transaction ->
+            val snapshot = transaction.get(sfDocRef)
 
-        // Note: this could be done without a transaction
-        //       by updating the population using FieldValue.increment()
-        transaction.update(sfDocRef, "supporter", user?.email)
+            // Note: this could be done without a transaction
+            //       by updating the population using FieldValue.increment()
+            transaction.update(sfDocRef, "supporter", user?.email)
 
-        // Success
-        null
-    }.addOnSuccessListener { Log.d(TAG, "Transaction success!") }
-        .addOnFailureListener { e -> Log.w(TAG, "Transaction failure.", e) }
-}
+            // Success
+            null
+        }.addOnSuccessListener { Log.d(TAG, "Transaction success!") }
+            .addOnFailureListener { e -> Log.w(TAG, "Transaction failure.", e) }
+    }
 
-fun loadData(id: String) {
+    fun countGiveup() {
+        val googleUser = FirebaseAuth.getInstance().currentUser
+        val email = googleUser?.email!!
+        val washingtonRef = db.collection("사용자").document("$email")
+        washingtonRef.update("giveUpCount", FieldValue.increment(1))
+    }
 
-    val docRef = db.collection("심부름").document("$id")
-    docRef.get()
-        .addOnSuccessListener { document ->
-            if (document != null) {
-                Log.d(TAG, "DocumentSnapshot data: ${document.data}")
-                tvName.text = document["name"].toString()
-                tvCategory.text = document["category"].toString()
-                tvContent.text = document["content"].toString()
-                tvLocation.text = document["location"].toString()
-                tvEmerg.text = document["urgencyDegree"].toString()
-                tvTitle.text = document["title"].toString()
-                tvPayment.text = document["payment"].toString()
-                sbremail = document["email"].toString()
-                sbrtitle = document["title"].toString()
-                sbrtoken = document["token"].toString()
-                sbrstate = document["state"].toString().toInt()
-                val uri = Uri.parse(document["photoUrl"].toString())
-                Glide.with(this).load(uri).into(ivDetailsProfile)
-                progressBar.visibility = View.GONE
-            } else {
-                Log.d(TAG, "No such document")
+    fun countComplete() {
+        val googleUser = FirebaseAuth.getInstance().currentUser
+        val email = googleUser?.email!!
+        val washingtonRef = db.collection("사용자").document("$email")
+        washingtonRef.update("comCount", FieldValue.increment(1))
+    }
+
+    fun loadData(id: String) {
+
+        val docRef = db.collection("심부름").document("$id")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+                    tvName.text = document["name"].toString()
+                    tvCategory.text = document["category"].toString()
+                    tvContent.text = document["content"].toString()
+                    tvLocation.text = document["location"].toString()
+                    tvEmerg.text = document["urgencyDegree"].toString()
+                    tvTitle.text = document["title"].toString()
+                    tvPayment.text = document["payment"].toString()
+                    sbremail = document["email"].toString()
+                    sbrtitle = document["title"].toString()
+                    sbrtoken = document["token"].toString()
+                    sbrstate = document["state"].toString().toInt()
+                    val uri = Uri.parse(document["photoUrl"].toString())
+                    Glide.with(this).load(uri).into(ivDetailsProfile)
+                    progressBar.visibility = View.GONE
+                } else {
+                    Log.d(TAG, "No such document")
+                }
             }
-        }
-        .addOnFailureListener { exception ->
-            Log.d(TAG, "get failed with ", exception)
-        }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
 
 
-}
+    }
 
     fun updategiveupSupporterOfErrand(id: String) {
         val user = FirebaseAuth.getInstance().currentUser
@@ -133,22 +150,22 @@ fun loadData(id: String) {
             .addOnFailureListener { e -> Log.w(TAG, "Transaction failure.", e) }
     }
 
-// 심부름에 서포터 정보 입력
-fun updategiveupErrandState(id: String) {
-    val sfDocRef = db.collection("심부름").document("$id")
+    // 심부름에 서포터 정보 입력
+    fun updategiveupErrandState(id: String) {
+        val sfDocRef = db.collection("심부름").document("$id")
 
-    db.runTransaction { transaction ->
-        val snapshot = transaction.get(sfDocRef)
+        db.runTransaction { transaction ->
+            val snapshot = transaction.get(sfDocRef)
 
-        // Note: this could be done without a transaction
-        //       by updating the population using FieldValue.increment()
-        transaction.update(sfDocRef, "state", 0)
+            // Note: this could be done without a transaction
+            //       by updating the population using FieldValue.increment()
+            transaction.update(sfDocRef, "state", 0)
 
-        // Success
-        null
-    }.addOnSuccessListener { Log.d(TAG, "Transaction success!") }
-        .addOnFailureListener { e -> Log.w(TAG, "Transaction failure.", e) }
-}
+            // Success
+            null
+        }.addOnSuccessListener { Log.d(TAG, "Transaction success!") }
+            .addOnFailureListener { e -> Log.w(TAG, "Transaction failure.", e) }
+    }
 
     fun updateErrandState(id: String) {
         val sfDocRef = db.collection("심부름").document("$id")
